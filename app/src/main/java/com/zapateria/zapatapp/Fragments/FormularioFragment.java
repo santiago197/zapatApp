2f1a25d53a09364c4ab6f9b3e2de5e8d7dc5abe2
Santiago197 <90514403+SantiagoRodriguezP@users.noreply.github.com>
2023-07-27 21:48:14 -0500

    Splash screen progress bar

---
+++ app/src/main/java/com/zapateria/zapatapp/Fragments/FormularioFragment.java
@@ -0,0 +1,7 @@
+package com.zapateria.zapatapp.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class FormularioFragment extends Fragment {
+
+}
--- app/src/main/java/com/zapateria/zapatapp/SplashActivity.java
+++ app/src/main/java/com/zapateria/zapatapp/SplashActivity.java
@@ -1,20 +1,26 @@
 package com.zapateria.zapatapp;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 
 public class SplashActivity extends Activity {
     private ProgressBar pb;
-    private static int SPLASH_DURATION = 5000;
+    private ImageView logo;
+    private static int SPLASH_DURATION = 6000;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        ImageView logo = findViewById(R.id.logo);
 
         pb = (ProgressBar)findViewById(R.id.progressBar);
         new LoadDataAsyncTask().execute();
@@ -28,6 +34,7 @@ public class SplashActivity extends Activity {
     }
     private void startMainActivity() {
         pb.setVisibility(View.INVISIBLE);
+
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish(); // Cierra esta actividad para que el usuario no pueda volver al splash screen presionando el botón "Atrás"
@@ -41,7 +48,7 @@ public class SplashActivity extends Activity {
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(SPLASH_DURATION);
                 ;
             } catch (InterruptedException e) {
                 e.printStackTrace();
